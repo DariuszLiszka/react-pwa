87340485414a9451f9110c99fc4c25250014328a
DariuszLiszka <dariusz.liszka96@gmail.com>
2020-03-21 21:22:34 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Fragment } from 'react';
-
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import SignUpForm from './components/signUpForm/SignUpForm';
 import NavBar from './components/navBar/NavBar';
-
+import Home from './components/home/Home';
 function App() {
   return (
-    <Fragment>
-      <NavBar />
-      <SignUpForm />
-    </Fragment>
+    <Router>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route path='/signup'>
+            <SignUpForm />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Link, BrowserRouter as Router } from 'react-router-dom';
 import styles from './NavBar.module.css';
 export default function NavBar() {
   return (
-    <div className={styles.NavContainer}>
-      <div className={styles.logo}>logo</div>
-      <nav>
-        <ul>
-          <li>Services</li>
-          <li>Gallery</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </div>
+    <Router>
+      <div className={styles.NavContainer}>
+        <div className={styles.logo}>logo</div>
+        <nav>
+          <ul>
+            <li>Sign up!</li>
+            <li>Gallery</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </div>
+    </Router>
   );
 }
